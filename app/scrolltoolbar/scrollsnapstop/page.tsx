3c5e7beb566d07e85008e82442b03e78e1fc7f64
Toby Liu <ybot1122@gmail.com>
2024-10-22 13:22:15 -0700

    100vh

--- app/scrolltoolbar/scrollsnapstop/page.tsx
+++ app/scrolltoolbar/scrollsnapstop/page.tsx
@@ -2,21 +2,23 @@ import RiveCar from "@/components/RiveCar/RiveCar";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] snap-y snap-mandatory">
+    <div className="w-[100vh] snap-y snap-mandatory">
       <div className="fixed top-0 left-0 w-full bg-yellow">I am fixed</div>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start snap-y snap-mandatory">
-          <h1 className="text-xl snap-start">Scroll Snap Scrolling</h1>
-          <h2>scroll-snap-type: y mandatory; scroll-snap-stop: always;</h2>
-          <p className="h-[50vh] snap-start bg-green snap-always">Start scrolling...</p>
-          <p className="h-[50vh] snap-start bg-yellow snap-always">Just taking up space</p>
-          <p className="h-[50vh] snap-start bg-green snap-always">So you can keep</p>
-          <p className="h-[50vh] snap-start bg-yellow snap-always">scrolling</p>
-          <p className="h-[50vh] snap-start bg-green snap-always">over and over</p>
-          <p className="h-[50vh] snap-start bg-yellow snap-always">more...</p>
-          <p className="h-[50vh] snap-start bg-green snap-always">over and over</p>
-          <p className="h-[50vh] snap-start bg-yellow snap-always">more more...</p>
-          <div className="h-[50vh] snap-start bg-green snap-always w-full">Car<RiveCar /></div>
-          <p className="h-[50vh] snap-start bg-yellow snap-always">end</p>
+      <main className="flex flex-col items-center sm:items-start snap-y snap-mandatory *:p-10">
+        <div className="h-[100vh] snap-start bg-yellow">
+            <h1 className="text-xl">Scroll Snap Scrolling</h1>
+            <h2>scroll-snap-type: y mandatory; scroll-snap-stop: always;</h2>
+        </div>
+          <p className="h-[100vh] snap-start bg-green snap-always">Start scrolling...</p>
+          <p className="h-[100vh] snap-start bg-yellow snap-always">Just taking up space</p>
+          <p className="h-[100vh] snap-start bg-green snap-always">So you can keep</p>
+          <p className="h-[100vh] snap-start bg-yellow snap-always">scrolling</p>
+          <p className="h-[100vh] snap-start bg-green snap-always">over and over</p>
+          <p className="h-[100vh] snap-start bg-yellow snap-always">more...</p>
+          <p className="h-[100vh] snap-start bg-green snap-always">over and over</p>
+          <p className="h-[100vh] snap-start bg-yellow snap-always">more more...</p>
+          <div className="h-[100vh] snap-start bg-green snap-always w-full">Car<RiveCar /></div>
+          <p className="h-[100vh] snap-start bg-yellow snap-always">end</p>
         </main>
     </div>
   );
