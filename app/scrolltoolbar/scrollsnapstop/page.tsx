c4b357605ef69544a33f307c0f0f8a334d6b49e3
Toby Liu <ybot1122@gmail.com>
2024-10-22 12:59:54 -0700

    with fixed

--- app/scrolltoolbar/scrollsnapstop/page.tsx
+++ app/scrolltoolbar/scrollsnapstop/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] snap-y snap-mandatory">
+      <div className="fixed top-0 left-0 w-full bg-yellow">I am fixed</div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start snap-y snap-mandatory">
           <h1 className="text-xl snap-start">Scroll Snap Scrolling</h1>
           <h2>scroll-snap-type: y mandatory; scroll-snap-stop: always;</h2>
