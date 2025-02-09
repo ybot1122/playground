9c7e933f4143d77432d7a2643f9b4a4470ca6780
Toby Liu <ybot1122@gmail.com>
2025-02-09 09:25:43 -0800

    iniital setup

---
+++ react-query-throw/src/pages/_app.js
@@ -0,0 +1,16 @@
+import "@/styles/globals.css";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+}
