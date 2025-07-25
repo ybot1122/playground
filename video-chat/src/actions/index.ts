79d6c7f715b53cb41f4000d2d39c5ad432a06e1b
Toby Liu <ybot1122@gmail.com>
2025-07-25 15:00:27 -0700

    IN-PROGRESS: Realtime one-way video chat with AWS Kinesis (#11)

---
+++ video-chat/src/actions/index.ts
@@ -0,0 +1,67 @@
+import { defineAction } from "astro:actions";
+import { z } from "astro:schema";
+import {
+  GetSignalingChannelEndpointCommand,
+  KinesisVideoClient,
+} from "@aws-sdk/client-kinesis-video";
+import {
+  KinesisVideoSignalingClient,
+  GetIceServerConfigCommand,
+} from "@aws-sdk/client-kinesis-video-signaling";
+
+export const server = {
+  getChannelEndpoints: defineAction({
+    input: z.object({}),
+    handler: async (input) => {
+      // 1. Get channel ARN
+      const kinesisVideoClient = new KinesisVideoClient({
+        region: import.meta.env.PUBLIC_AWS_REGION,
+        credentials: {
+          accessKeyId: import.meta.env.PUBLIC_AWS_ACCESS_KEY,
+          secretAccessKey: import.meta.env.PUBLIC_AWS_SECRET_ACCESS_KEY,
+        },
+      });
+
+      // 2. Get endpoints
+      const getEndpointRes = await kinesisVideoClient.send(
+        new GetSignalingChannelEndpointCommand({
+          ChannelARN: import.meta.env.PUBLIC_AWS_CHANNEL_ARN,
+          SingleMasterChannelEndpointConfiguration: {
+            Protocols: ["WSS", "HTTPS"],
+            Role: "MASTER",
+          },
+        }),
+      );
+      const endpointsByProtocol: Record<string, string> = {};
+      (getEndpointRes.ResourceEndpointList || []).forEach((endpoint) => {
+        if (endpoint.Protocol && endpoint.ResourceEndpoint) {
+          endpointsByProtocol[endpoint.Protocol] = endpoint.ResourceEndpoint;
+        }
+      });
+
+      // 3. Get ICE Server Config
+      try {
+        const signalingClient = new KinesisVideoSignalingClient({
+          region: import.meta.env.PUBLIC_AWS_REGION,
+          endpoint: endpointsByProtocol.HTTPS,
+          credentials: {
+            accessKeyId: import.meta.env.PUBLIC_AWS_ACCESS_KEY,
+            secretAccessKey: import.meta.env.PUBLIC_AWS_SECRET_ACCESS_KEY,
+          },
+        });
+
+        const iceServersResponse = await signalingClient.send(
+          new GetIceServerConfigCommand({
+            ChannelARN: import.meta.env.PUBLIC_AWS_CHANNEL_ARN,
+          }),
+        );
+
+        console.log(iceServersResponse);
+      } catch (e) {
+        console.log(e);
+      }
+
+      return endpointsByProtocol;
+    },
+  }),
+};
