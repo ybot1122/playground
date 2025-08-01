187103739d012ad6fb6af8f84ab40134e0c73e55
Toby Liu <ybot1122@gmail.com>
2025-08-01 14:59:28 -0700

    week buckets?

--- vega-lite/dist/script.d.ts
+++ vega-lite/dist/script.d.ts
@@ -2,6 +2,7 @@ interface DataPoint {
     category: string;
     value: number;
 }
+declare const createDivRenderSpec: (divId: string, spec: any) => void;
 declare const spec: {
     $schema: string;
     description: string;
@@ -26,4 +27,60 @@ declare const spec: {
         };
     };
 };
-declare let container: HTMLElement | null;
+declare const seattleWeather: {
+    data: {
+        url: string;
+    };
+    mark: string;
+    encoding: {
+        x: {
+            timeUnit: string;
+            field: string;
+            type: string;
+            title: string;
+        };
+        y: {
+            aggregate: string;
+            type: string;
+        };
+        color: {
+            field: string;
+            type: string;
+            scale: {
+                domain: string[];
+                range: string[];
+            };
+            title: string;
+        };
+    };
+};
+declare const seattleWeatherOverTime: {
+    data: {
+        url: string;
+    };
+    mark: string;
+    encoding: {
+        x: {
+            timeUnit: string;
+            field: string;
+            type: string;
+            title: string;
+        };
+        y: {
+            aggregate: string;
+            type: string;
+        };
+        color: {
+            field: string;
+            type: string;
+            scale: {
+                domain: string[];
+                range: string[];
+            };
+            title: string;
+        };
+    };
+};
+declare function readFileSyncNoLibs(path: string): any;
+declare function csvToJson(csv: string): any[];
+declare const renderMyStuff: () => Promise<void>;
